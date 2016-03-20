171141a29787d8103183d946d47812dd68306e6e
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2016-03-20 16:10:39 +0530

    Added Form Validation on expense form modal

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','ngCordova'])
+angular.module('starter', ['ionic','ngCordova','ngMessages'])
 
 .run(function($ionicPlatform,$cordovaSQLite) {
   $ionicPlatform.ready(function() {
@@ -80,10 +80,67 @@ angular.module('starter', ['ionic','ngCordova'])
 
 
 
+//Directives for validation
+
+.directive('onValidSubmit', ['$parse', '$timeout', function($parse, $timeout) {
+    return {
+      require: '^form',
+      restrict: 'A',
+      link: function(scope, element, attrs, form) {
+        form.$submitted = false;
+        var fn = $parse(attrs.onValidSubmit);
+        element.on('submit', function(event) {
+          scope.$apply(function() {
+            element.addClass('ng-submitted');
+            form.$submitted = true;
+            if (form.$valid) {
+              if (typeof fn === 'function') {
+                fn(scope, {$event: event});
+              }
+            }
+          });
+        });
+      }
+    }
+ 
+  }])
+
+//Directives for validation
+
+  .directive('validated', ['$parse', function($parse) {
+    return {
+      restrict: 'AEC',
+      require: '^form',
+      link: function(scope, element, attrs, form) {
+        var inputs = element.find("*");
+        for(var i = 0; i < inputs.length; i++) {
+          (function(input){
+            var attributes = input.attributes;
+            if (attributes.getNamedItem('ng-model') != void 0 && attributes.getNamedItem('name') != void 0) {
+              var field = form[attributes.name.value];
+              if (field != void 0) {
+                scope.$watch(function() {
+                  return form.$submitted + "_" + field.$valid;
+                }, function() {
+                  if (form.$submitted != true) return;
+                  var inp = angular.element(input);
+                  if (inp.hasClass('ng-invalid')) {
+                    element.removeClass('has-success');
+                    element.addClass('has-error');
+                  } else {
+                    element.removeClass('has-error').addClass('has-success');
+                  }
+                });
+              }
+            }
+          })(inputs[i]);
+        }
+      }
+    }
+  }])
 
 
-
-
+//controllers for functionality
 
 .controller('ListController',function($scope,$http,$cordovaSQLite,$ionicModal){
 
